2f84913c8b9646c4f3f5869a49dee49a8cc6d898
Rodas-Nega <71346086+Rodas-Nega@users.noreply.github.com>
2020-09-28 10:35:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
+// Created By: Rodas N.
+// Created On: Sept 28 2020
+// 
+// An addition/subtraction program ran by inputs on the set variable, "counter".
+let counter = 0
+counter = 0
